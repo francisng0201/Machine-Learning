fc0fcc382465bdb7cd02c901803c87ee8673266d
francisng0201 <cng10@illinois.edu>
2016-05-05 18:28:05 -0500

    First commit

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +26,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
 #########################################################
 
 
