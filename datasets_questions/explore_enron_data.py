61de1737899922608ad888ad16d6641001b30983
francisng0201 <cng10@illinois.edu>
2016-05-16 01:10:52 -0500

    finish decision_tree and svm

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,28 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print("number of data points: %r" % len(enron_data))
+print("number of features: %r" % len(enron_data["SKILLING JEFFREY K"]))
 
+POI = 0
+for i in enron_data:
+	if (enron_data[i]["poi"] == 1):
+		POI+=1
+
+print("number of POIs: %r" % POI)
+print("total value of the stock belonging to James Prentice: %r" % enron_data["PRENTICE JAMES"]["total_stock_value"])
+print("email messages from Wesley Colwell to persons of interest: %r" % enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+print("value of stock options exercised by Jeffrey Skilling: %r" % enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+max_money = max(enron_data["SKILLING JEFFREY K"]["total_payments"], enron_data["LAY KENNETH L"]["total_payments"], enron_data["FASTOW ANDREW S"]["total_payments"])
+print("the one who took the most money: %r" % max_money)
+
+salary = 0
+email = 0
+for i in enron_data:
+	if (enron_data[i]["salary"] != "NaN"):
+		salary += 1
+	if (enron_data[i]["email_address"] != "NaN"):
+		email += 1
+
+print("folks that have a quantified salary: %r" % salary)
+print("folks that have a quantified email: %r" % email)
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,7 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+#clf = svm.SVC(kernel = "rbf", C = 10)
+#clf.fit(features_train, labels_train)
+#print(clf.score(features_test, labels_test))
+#clf = svm.SVC(kernel = "rbf", C = 100)
+#clf.fit(features_train, labels_train)
+#print(clf.score(features_test, labels_test))
+#clf = svm.SVC(kernel = "rbf", C = 1000)
+#clf.fit(features_train, labels_train)
+#print(clf.score(features_test, labels_test))
+clf = svm.SVC(kernel = "rbf", C = 10000)
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
 
+pred = clf.predict(features_test)
+print("10th: %r, 26th: %r, 50th: %r" % (pred[10], pred[26], pred[50]))
+print("in Chris: %r" % sum(pred))
 #########################################################
 
 
